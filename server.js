f8871c1db3556cc40e01e8f1fefdaeb14043a562
bdwxxx <bogbldan@gmail.com>
2025-07-28 19:37:39 +0300

    init

---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const app = express();
+app.use(express.json());
+
+app.post('/create-checkout-session', async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [{
+        price: 'price_1RprmJCmyfYBnltweq17ZRg6',
+        quantity: 1,
+      }],
+      mode: 'subscription',
+      billing_address_collection: 'required',
+      success_url: 'https://retroemu.me/?payment=success',
+      cancel_url: 'https://retroemu.me/?payment=cancel',
+    });
+    res.json({ sessionId: session.id });
+  } catch (e) {
+    console.error("Stripe Error:", e.message);
+    res.status(500).send({ error: { message: e.message } });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Backend server running on http://localhost:${PORT}`));
